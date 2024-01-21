59af3357206c5278dbfac8c7fcae2ea4ce1f076c
nicosharpe <nicolesharpe11@gmail.com>
2024-01-21 12:36:34 +0000

    ADDED PSEUDOCODE

---
+++ script.js
@@ -0,0 +1,9 @@
+// display current date at the top of page/calendar 
+// USE UTILITY LIBRARY
+// time blocks for standard business hrs
+// colour code each time block based on past/present/future when viewed
+// allow user to enter an event when they click time block
+// save event in local storage when save button is clicked in TB
+// PERSIST EVENTS events stay on page after refresh
+
+
