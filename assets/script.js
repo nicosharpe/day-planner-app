419beefbcb8bbc04b4f6d160b9680d017cdd62c2
nicosharpe <nicolesharpe11@gmail.com>
2024-01-23 22:17:48 +0000

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,53 @@
-// display current date at the top of page/calendar - DONE
-// USE UTILITY LIBRARY - DONE
-// time blocks for standard business hrs - hmm?
-// colour code each time block based on past/present/future when viewed
-// allow user to enter an event when they click time block
-// save event in local storage when save button is clicked in TB
-// PERSIST EVENTS events stay on page after refresh
-
 $(document).ready(function () {
-
     var today = dayjs().format("dddd D MMMM YYYY");
-    var todaysTime = dayjs().format("hh:mm a")
-    var hour = 8; hour <= 18; hour++
-    var hourFormat = dayjs().hour(hour).format("H A");
-    var time = $("<div>").addClass("time-block row hour col-1 py-3").text(hourFormat);
-    var textArea = $("<input>").addClass("textarea col-10").text("");
-    var saveButton = $("<button>").addClass("saveBtn col-1 fa-solid fa-floppy-disk");
+    var todaysTime = dayjs().format("h:mm a");
 
-    // display todays date
     function displayDate() {
-
         $("#currentDay").text(today);
         $("#currentTime").text(todaysTime);
     }
     displayDate();
 
-    //create rows for each hour with col elements for time, text and save btn
     function timeBlocks() {
-
         var container = $("#timeblocks-container");
 
-        // Loop from 8am to 6pm (24-hour format)
         for (let hour = 8; hour <= 18; hour++) {
-
             var row = $("<div>").addClass("row mx-auto");
-
             container.append(row);
 
             var hourFormat = dayjs().hour(hour).format("h A");
-
             var time = $("<div>").addClass("time-block row hour col-1 py-3").text(hourFormat);
-
             row.append(time);
-
-            var textArea = $("<input>").addClass("textarea col-10").text("");
-
+            var textArea = $("<input>").addClass("textarea col-10").attr("id", hour);
             row.append(textArea);
-
             var saveButton = $("<button>").addClass("saveBtn col-1 fa-solid fa-floppy-disk");
-
             row.append(saveButton);
 
             changeColour(hour, textArea);
-        }
+            loadSavedEvents(hour, textArea);
 
+            // Set the hour value as a data attribute
+            saveButton.data("hour", hour);
+
+console.log(textArea);
+
+
+            saveButton.on("click", function () {
+                var id= $(textArea).attr("id")
+                console.log(id);
+             
+                console.log(textArea.val())
+                // Retrieve the hour value from the data attribute
+                var clickedHour = $(this).data("hour");
+                saveEvent(clickedHour, textArea.val());
+                console.log(clickedHour);
+            });
+        }
     }
     timeBlocks();
 
-
     function changeColour(hour, textArea) {
-        var currentHour = dayjs().hour()
+        var currentHour = dayjs().hour();
 
         if (hour < currentHour) {
             textArea.addClass("past").removeClass("present future");
@@ -69,20 +58,34 @@ $(document).ready(function () {
         }
     }
 
+    function saveEvent(hour, eventText) {
+        
+        var events = JSON.parse(localStorage.getItem("events")) || [];
 
+        var existingEvent = events.find(function (event) {
 
+            return event.hour === hour;
+        });
 
+        if (existingEvent) {
+            existingEvent.text = eventText;
+        } else {
+            events.push({ hour: hour, text: eventText });
+        }
 
+        localStorage.setItem("events", JSON.stringify(events));
+    }
 
+    function loadSavedEvents(hour, textArea) {
+        var events = JSON.parse(localStorage.getItem("events")) || [];
+        var event = events.find(function (event) {
+            return event.hour === hour;
+        });
 
-
-
-
-
-   
-
-
-
+        if (event) {
+            textArea.val(event.text);
+        }
+    }
 
 
 });
